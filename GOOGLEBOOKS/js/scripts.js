26a274258a2d9a88bf8adb939ca750ab9bb1c0b8
Hidran Arias <hidran@gmail.com>
2019-08-13 22:41:49 +0200

    adds search books

--- GOOGLEBOOKS/js/scripts.js
+++ GOOGLEBOOKS/js/scripts.js
@@ -5,8 +5,8 @@ function getBooks(booktitle) {
     var apiurl = 'https://www.googleapis.com/books/v1/volumes?q=';
     var p = fetch(apiurl + booktitle).then(function (res) { return res.json(); });
     // .then( books => console.log(books))
-    from(p)
-        .pipe(switchMap(function (data) { return from(data.items); }), map(function (ele) {
+    return from(p)
+        .pipe(tap(function (data) { return showTotal(data.items.length); }), switchMap(function (data) { return from(data.items || []); }), map(function (ele) {
         var book = {
             title: ele.volumeInfo.title,
             categories: ele.volumeInfo.categories,
@@ -15,7 +15,46 @@ function getBooks(booktitle) {
             thumbnail: ele.volumeInfo.imageLinks.thumbnail
         };
         return book;
-    }), tap(function (book) { return console.log(book); }))
-        .subscribe(function (data) { return data; });
+    }), tap(function (book) { return console.log(book); }));
 }
-getBooks('game of 3  thrones');
+function showTotal(total) {
+    console.log(total);
+    var found = document.querySelector('#found');
+    if (found) {
+        found.textContent = '' + total;
+    }
+}
+function displayBook(book) {
+    var bookTpl = "\n    <div class=\"card mb-4 shadow-sm\">  \n        \n        <img src=\"" + book.thumbnail + "\" title=\"" + book.title + "\"  alt=\"" + book.title + "\">\n      <div class=\"card-body\">\n      <h5>" + book.title + "</h5>\n        <p class=\"card-text\"></p>\n        <div class=\"d-flex justify-content-between align-items-center\">\n          <div class=\"btn-group\">\n            <button type=\"button\" class=\"btn btn-sm btn-outline-secondary\">View</button>\n            <button type=\"button\" class=\"btn btn-sm btn-outline-secondary\">Edit</button>\n          </div>\n          <small class=\"text-muted\">9 mins</small>\n        </div>\n      </div>\n    </div>\n  </div>\n";
+    var div = document.createElement('div');
+    div.setAttribute('class', 'col-md-3');
+    div.innerHTML = bookTpl;
+    var books = document.querySelector('#books');
+    if (books) {
+        books.appendChild(div);
+    }
+}
+function cleanBooks() {
+    var books = document.querySelector('#books');
+    if (books) {
+        books.innerHTML = '';
+    }
+}
+function searchBooks() {
+    var searchEle = document.querySelector('#search');
+    var fromEvent = rxjs.fromEvent;
+    var _a = rxjs.operators, filter = _a.filter, map = _a.map, switchMap = _a.switchMap, debounceTime = _a.debounceTime, tap = _a.tap;
+    if (searchEle) {
+        fromEvent(searchEle, 'keyup')
+            .pipe(map(function (ele) { return ele.target.value; }), filter(function (ele) { return ele.length > 2; }), debounceTime(1200), tap(function () { return cleanBooks(); }), switchMap(function (ele) { return getBooks(ele); }))
+            .subscribe(function (book) { return displayBook(book); });
+        //  getBooks('game of 3  thrones');
+    }
+}
+searchBooks();
+function searchButtonClicked() {
+    var books = document.querySelector('#search');
+    if (books) {
+        getBooks(books.value).subscribe(function (book) { return displayBook(book); });
+    }
+}
--- GOOGLEBOOKS/ts/index.ts
+++ GOOGLEBOOKS/ts/index.ts
@@ -1,4 +1,5 @@
 declare const rxjs: any;
+declare var document: Document;
 interface GoogleBook {
     totalItems: number
     kind: string
@@ -30,16 +31,17 @@ interface BookItem{
     volumeInfo: VolumeInfo
     id: string
 }
-function getBooks(booktitle: string){
+ function getBooks(booktitle: string){
     const {from } = rxjs;
     const {map, switchMap, tap} = rxjs.operators;
     let apiurl = 'https://www.googleapis.com/books/v1/volumes?q=';
 
    const  p = fetch(apiurl + booktitle).then(res => res.json());
   // .then( books => console.log(books))
- from(p)
+ return from(p)
  .pipe(
-     switchMap((data:GoogleBook) => from(data.items)),
+     tap( (data:GoogleBook) => showTotal(data.items.length)),
+     switchMap((data:GoogleBook) => from(data.items || [])),
     
   
      map( (ele:BookItem) => {
@@ -53,9 +55,81 @@ function getBooks(booktitle: string){
       return book;
     }
      ),
-     tap( (book:Book) => console.log(book)),
+     tap( (book:Book) => console.log(book))
  )
- .subscribe((data:GoogleBook) =>  data )
+ 
  
 }
-getBooks('game of 3  thrones');
+function showTotal(total: number){
+    console.log(total)
+    const found = document.querySelector('#found');
+ if(found){
+    found.textContent = ''+total;
+ }
+}
+function displayBook(book: Book){
+    const bookTpl = `
+    <div class="card mb-4 shadow-sm">  
+        
+        <img src="${book.thumbnail}" title="${book.title}"  alt="${book.title}">
+      <div class="card-body">
+      <h5>${book.title}</h5>
+        <p class="card-text"></p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+          </div>
+          <small class="text-muted">9 mins</small>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
+const div = document.createElement('div');
+div.setAttribute('class','col-md-3');
+div.innerHTML = bookTpl;
+ const books = document.querySelector('#books');
+ if(books){
+    books.appendChild(div);
+ }
+ 
+
+}
+function cleanBooks(){
+  
+    const books = document.querySelector('#books');
+    if(books){
+       books.innerHTML ='';
+    }  
+}
+function searchBooks(){
+    const searchEle = document.querySelector('#search');
+     const {fromEvent } = rxjs;
+     const { filter, map, switchMap, debounceTime, tap} = rxjs.operators;
+    if(searchEle){
+        fromEvent(searchEle,'keyup')
+           .pipe(
+             
+               map( (ele: any ) => ele.target.value),
+              filter( (ele: string )  => ele.length >2),
+              debounceTime(1200),
+                tap( () => cleanBooks()),
+              switchMap((ele: string) => getBooks(ele) )
+           )
+       
+            .subscribe((book:Book) => displayBook(book))
+        
+      //  getBooks('game of 3  thrones');
+
+    }
+  
+}
+searchBooks();
+function searchButtonClicked(){
+    const books:any = document.querySelector('#search');
+    if(books){
+        getBooks(books.value) .subscribe((book:Book) => displayBook(book))
+    } 
+}
+
